8dc96837a48da59b30386f3fa0d922386b077010
Brendan Raykoff <99614905+Braykoff@users.noreply.github.com>
2026-07-17 18:44:20 -0400

    Fixed header and footer links

--- theme/navigation/data/footer.js
+++ theme/navigation/data/footer.js
@@ -9,12 +9,12 @@ export const siteNavigation = [
   // Featured
   {
     label: 'Featured',
-    url: '/featured',
+    url: '/featured/',
     children: [
-      { label: 'Special Issues', url: '/featured/special-issues' },
-      { label: "Editor's Picks", url: '/featured/editors-picks' },
-      { label: 'Awards', url: '/featured/awards' },
-      { label: 'Photo Essays', url: '/multimedia/photo-essays' },
+      { label: 'Special Issues', url: '/featured/special-issues/' },
+      { label: "Editor's Picks", url: '/featured/editors-picks/' },
+      { label: 'Awards', url: '/featured/awards/' },
+      { label: 'Photo Essays', url: '/multimedia/photo-essays/' },
     ],
   },
 
@@ -25,10 +25,10 @@ export const siteNavigation = [
     children: [
       { label: 'Campus', url: '/news/campus/' },
       { label: 'City', url: '/news/city/' },
-      { label: 'County', url: '/news/county/' },
+      { label: 'County', url: '/news/alameda-county/' },
       { label: 'State', url: '/news/state/' },
       { label: 'National', url: '/news/national/' },
-      { label: 'Obituaries', url: '/news/obituaries/' },
+      { label: 'Obituaries', url: '/news/obituary-news/' },
       { label: 'Investigations', url: '/news/investigations/' },
     ],
   },
@@ -36,7 +36,7 @@ export const siteNavigation = [
   // Sports
   {
     label: 'Sports',
-    url: '/sports',
+    url: '/sports/',
     children: [
       { label: 'Football', url: '/sports/msports/football/' },
       { label: 'M. Hoops', url: '/sports/msports/basketball/' },
@@ -44,41 +44,41 @@ export const siteNavigation = [
       { label: 'Baseball', url: '/sports/msports/baseball/' },
       { label: 'Softball', url: '/sports/wsports/softball/' },
       { label: 'Bear Bytes', url: '/sports/bearbytes/' },
-      { label: 'Columns', url: '/sports/sports-columns' },
-      { label: 'Special Report', url: '/sports/special-report' },
+      { label: 'Columns', url: '/sports/sports-columns/' },
+      { label: 'Special Report', url: '/sports/special-report/' },
     ],
   },
 
   // Arts
   {
     label: 'Arts',
-    url: '/arts',
+    url: '/arts/',
     children: [
-      { label: 'Music', url: '/arts/music' },
-      { label: 'Film & Television', url: '/arts/film-and-television' },
-      { label: 'Theater', url: '/arts/theater' },
-      { label: 'Visual Art', url: '/arts/visual-art' },
-      { label: 'Literature', url: '/arts/books' },
-      { label: 'Fashion', url: '/arts/fashion-arts' },
-      { label: 'Columns', url: '/arts/art-columns' },
-      { label: 'Culture Shot', url: '/blogs/arts-blog' },
-      { label: 'Video Games', url: '/arts/video-games' },
-      { label: 'Comedy', url: '/arts/comedy-arts' },
-      { label: 'Local Events', url: '/arts/local-event' },
-      { label: 'Arts Awards', url: '/arts/arts-awards' },
+      { label: 'Music', url: '/arts/music/' },
+      { label: 'Film & Television', url: '/arts/film-and-television/' },
+      { label: 'Theater', url: '/arts/theater/' },
+      { label: 'Visual Art', url: '/arts/visual-art/' },
+      { label: 'Literature', url: '/arts/books/' },
+      { label: 'Fashion', url: '/arts/fashion-arts/' },
+      { label: 'Columns', url: '/arts/art-columns/' },
+      { label: 'Culture Shot', url: '/arts/culture-shot/' },
+      { label: 'Video Games', url: '/arts/video-games/' },
+      { label: 'Comedy', url: '/arts/comedy-arts/' },
+      { label: 'Local Events', url: '/arts/local-event/' },
+      { label: 'Arts Awards', url: '/arts/arts-awards/' },
     ],
   },
 
   // Opinion
   {
     label: 'Opinion',
-    url: '/opinion',
+    url: '/opinion/',
     children: [
-      { label: 'Editorials', url: '/opinion/editorials' },
-      { label: 'Op-Eds', url: '/opinion/op-eds' },
-      { label: 'Letters to the Editor', url: '/opinion/letters-to-the-editor' },
-      { label: 'Columns', url: '/opinion/opinion-columns' },
-      { label: 'Editorial Cartoons', url: '/opinion/cartoons' },
+      { label: 'Editorials', url: '/opinion/editorials/' },
+      { label: 'Op-Eds', url: '/opinion/op-eds/' },
+      { label: 'Letters to the Editor', url: '/opinion/letters-to-the-editor/' },
+      { label: 'Columns', url: '/opinion/opinion-columns/' },
+      { label: 'Editorial Cartoons', url: '/cartoons/' },
     ],
   },
 
@@ -97,28 +97,28 @@ export const siteNavigation = [
   // Weekender
   {
     label: 'Weekender',
-    url: '/weekender',
+    url: '/weekender/',
     children: [
-      { label: 'Back & Forth', url: '/weekender/back_and_forth' },
+      { label: 'Back & Forth', url: '/weekender/back-and-forth/' },
       { label: 'Commentary', url: '/weekender/commentary/' },
-      { label: 'Features', url: '/weekender/features' },
-      { label: 'Personal Essays', url: '/weekender/personal_essays' },
-      { label: 'Poems', url: '/weekender/poems' },
-      { label: 'Fiction', url: '/weekender/fiction' },
+      { label: 'Features', url: '/weekender/features/' },
+      { label: 'Personal Essays', url: '/weekender/personal-essays/' },
+      { label: 'Poems', url: '/weekender/poems/' },
+      { label: 'Fiction', url: '/weekender/fiction/' },
     ],
   },
 
   // Multimedia
   {
     label: 'Multimedia',
-    url: '/multimedia',
+    url: '/multimedia/',
     children: [
-      { label: 'News: City', url: '/multimedia/multimedia-newscity' },
-      { label: 'News: Campus', url: '/multimedia/multimedia-newscampus' },
-      { label: 'Sports', url: '/multimedia/sports-multimedia' },
-      { label: 'Arts', url: '/multimedia/arts-multimedia' },
-      { label: 'Entertainment', url: '/multimedia/entertainment-multimedia' },
-      { label: 'Insider', url: '/multimedia/opinion-multimedia' },
+      { label: 'News: City', url: '/multimedia/' },
+      { label: 'News: Campus', url: '/multimedia/' },
+      { label: 'Sports', url: '/multimedia/' },
+      { label: 'Arts', url: '/multimedia/' },
+      { label: 'Entertainment', url: '/multimedia/' },
+      { label: 'Insider', url: '/multimedia/' },
     ],
   },
 
